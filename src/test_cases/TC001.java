3ab9f3cd64b0abef257e1c7f97c7fd8ccf2ae7c6
Aman <109422024+Amannaza@users.noreply.github.com>
2022-12-29 03:53:41 +0300

    updated TC001

--- src/test_cases/TC001.java
+++ src/test_cases/TC001.java
@@ -1,1 +1,1 @@
-package test_cases;public class TC001 {    public static void main(String[] args) {        //Testing some thing    }}
+package test_cases;public class TC001 {    public static void main(String[] args) {        //Testing some thing        //more testing    }}
